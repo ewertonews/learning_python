c8113fad3692557fefd06c3b29e801fddb51c266
Ewerton Willams <ewertonws@gmail.com>
2021-09-08 21:36:50 -0300

    first commit with some initial exercises

---
+++ numbers.py
@@ -0,0 +1,23 @@
+# result = 2 + 30 - 9
+# print(result * 2)
+# result = result / 2
+# print(result)
+#in python 2 the division always resturns a whole number
+#print(type(result))
+
+#MODULO - Find the remainder of a division
+print(19 % 3)
+
+#ORDERING OPERATORS - Parenthesis, Order (power), Division, Multiplication, Addition, Subtraction
+brackets = 2 * (5 - 1)
+print("2 * (5 - 1): ", brackets)
+power = 2 ** 3 * 2
+print("2 ** 3 * 2: ", power) #** = power
+
+print("Rounding numbers")
+print("rounding 2.3")
+
+import math
+print(round(2.3))
+print("It always round down. Use math.ceil to round up (2.5)")
+print(math.ceil(2.5))
---
+++ strings.py
@@ -0,0 +1,17 @@
+hello = "Hello" + " World! "
+print(hello)
+
+print(hello * 3)
+
+print("="*50)
+
+a = "part "
+b = 1
+print(a + str(b))
+
+# Format
+book = "Jonh"
+chapter = 3
+verse = 16
+string_formated = "{} {}:{}".format(book, chapter, verse)
+print(string_formated)
