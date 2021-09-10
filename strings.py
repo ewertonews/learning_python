54ae9bc9b5b3d67f66c8e6372b091b8b4c968c94
Ewerton Willams <ewertonws@gmail.com>
2021-09-10 09:00:54 -0300

    Added multiline string to strings.py

--- strings.py
+++ strings.py
@@ -15,3 +15,17 @@ chapter = 3
 verse = 16
 string_formated = "{} {}:{}".format(book, chapter, verse)
 print(string_formated)
+
+#multiline string
+multiline = """
+lorem ipsum dolor sit amet 
+and the rest I don't know
+=)"""
+
+anothermultiline = '''
+We can write
+a paragraph
+here'''
+
+print(multiline)
+print(anothermultiline)
